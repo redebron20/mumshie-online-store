71df98c4ce94ed52bfc71529fb6632ea0479d397
redebron20 <redebron20@gmail.com>
2021-04-07 22:01:40 -0400

    Set up has secure password and add bcrypt gem.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     class User < ApplicationRecord
+        has_secure_password
+        
         has_many :orders
         has_many :quantities
         has_many :products, through: :quantities
