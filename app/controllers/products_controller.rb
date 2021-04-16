ad661cb0785d410b93ab891b824e94e37965cee1
redebron20 <redebron20@gmail.com>
2021-04-16 12:32:53 -0400

    Create controller methods and index form for products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,19 @@
 class ProductsController < ApplicationController
     def index
+        @products = Product.all.order(:name)
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+
+        else
+
+        end
+
     end
 end
