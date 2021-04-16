144bbf1b9f634e0fa5ba818234661c69808f68ba
redebron20 <redebron20@gmail.com>
2021-04-16 07:42:09 -0400

    Add login methods to find user in the database using email and authenticate with user password.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,16 @@ class SessionsController < ApplicationController
         @user = User.new
     end
 
+    def create
+        user = User.find_by_email(params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+          session[:user_id] = user.id
+          redirect_to user
+        else
+          #set error message with flash
+          redirect_to '/login', error: "Invalid credentials"
+        end
+      end
     
 
 end
