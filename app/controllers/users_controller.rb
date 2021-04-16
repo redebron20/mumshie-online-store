5efa8414214af8cfa801529c75b5a60c767e6acd
redebron20 <redebron20@gmail.com>
2021-04-16 07:39:09 -0400

    Create methods for new user signup and helper methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,28 @@
 class UsersController < ApplicationController
     before_action :redirect_if_logged_in, only: [:new]
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(signup_params)
+        if @user.id
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = current_user
+    end
+
+    private
+
+    def signup_params
+        params.require(:user).permit(:email, :username, :password)
+    end
+
 end
