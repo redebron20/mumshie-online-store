e029725802621d3f9860ab2d0b6d214257c29c8f
redebron20 <redebron20@gmail.com>
2021-04-24 23:27:22 -0400

    Fix routes for login and signup actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,26 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'products#homepage'
-  # get 'auth/:provider/callback', to: 'sessions#omniauth'
-  # get '/users/reset_password', to: 'users#reset_form'
-  # post '/users/reset_password', to: 'users#password_reset'
+  resources :categories do
+    resources :products, only: [:index, :new, :create]
+  end
+
+  resources :users, only: [:index, :show]
+  resources :admins
+
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   get '/logout', to: "sessions#destroy"
 
-  # get '/products', to: 'products#index'
-  # get '/products/:id', to: 'products#show', as: 'product'
-  # get '/products/new', to: 'products#new'
-  # post '/products', to: 'products#create'
-
-  #shortcut route generator syntax
-  resources :products, only: [:index, :new, :create]
-  resources :users, only: [:index, :show]
-  resources :brands do
-    resources :products, shallow: true
-  end
-    
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/show'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'products#homepage'
+  # get 'auth/:provider/callback', to: 'sessions#omniauth'
+  # get '/users/reset_password', to: 'users#reset_form'
+  # post '/users/reset_password', to: 'users#password_reset'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
