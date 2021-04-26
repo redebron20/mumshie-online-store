2d4040076f34dbed346ace599576655b4672f3cc
redebron20 <redebron20@gmail.com>
2021-04-26 11:43:01 -0400

    Create index form to show category list.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,7 +20,7 @@ class CategoriesController < ApplicationController
     end
 
     def show
-        category = Category.find_by(id: params[:id])
+        @category = Category.find_by(id: params[:id])
     end
 
     private
