9e1d819186086a7f6ce5530668a65fd791c1a58b
redebron20 <redebron20@gmail.com>
2021-04-27 08:53:08 -0400

    Create show form for each product.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,17 +2,7 @@ class ProductsController < ApplicationController
     before_action :find_product, only: :show
     
     def index
-        if params[:category].blank?
-            @product = Product.all.order(params[:sort])
-        else
-            @category_id = Category.find_by(name: params[:category]).id
-            if params[:sort]
-                @product = Product.where(category_id: @category_id).order(params[:sort])
-            else
-                @product = Product.where(category_id: @category_id).order("created_at ASC")
-            end
-            @category = Category.find(@category_id)
-         end
+        @products = Product.all
     end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
   resources :admins
+  resources :products
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
