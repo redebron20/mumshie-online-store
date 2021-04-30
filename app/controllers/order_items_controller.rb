c70ec62ee273846832493594f7ad668ad4300ec5
redebron20 <redebron20@gmail.com>
2021-04-30 09:20:06 -0400

    Create CRUD actions to order_items controller to increment and decrement order items quantity.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -18,12 +18,29 @@ class OrderItemsController < ApplicationController
       
         # Save and redirect to order show path
         @order_item.save
-        byebug
         redirect_to order_path(current_order)
       end
+
+      def destroy
+        @order_item = OrderItem.find(params[:id])
+        @order_item.destroy
+        redirect_to cart_path(@current_cart)
+      end  
+
+      def add_quantity
+        @order_item = OrderItem.find(params[:id])
+        @order_item.quantity += 1
+        @order_item.save
+        redirect_to cart_path(@current_cart)
+      end
       
-      private
-        def order_item_params
-          params.require(:order_item).permit(:quantity,:product_id, :order_id)
+      def reduce_quantity
+        @order_item = OrderItem.find(params[:id])
+        if @order_item.quantity > 1
+          @order_item.quantity -= 1
         end
+        @order_item.save
+        redirect_to cart_path(@current_cart)
+      end
+      
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
-    has_many :order_items
+    has_many :order_items, dependent: :destroy
     has_many :products, through: :order_items
 
     def sub_total
