6e25f297463317754ee9dc0b746c2a0828255c4d
redebron20 <redebron20@gmail.com>
2021-04-30 09:36:51 -0400

    Check for typo error and fix bugs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,22 @@ class ApplicationController < ActionController::Base
     include ApplicationHelper
 
     add_flash_types :success, :error, :warning, :info
-    before_action :current_order
+    before_action :current_cart
 
     private
-    def current_order
-      def current_cart
-        if session[:cart_id]
-          cart = Cart.find_by(:id => session[:cart_id])
-          if cart.present?
-            @current_cart = cart
-          else
-            session[:cart_id] = nil
-          end
+    def current_cart
+      if session[:cart_id]
+        cart = Cart.find_by(:id => session[:cart_id])
+        if cart.present?
+          @current_cart = cart
+        else
+          session[:cart_id] = nil
         end
-  
-        if session[:cart_id] == nil
-          @current_cart = Cart.create
-          session[:cart_id] = @current_cart.id
-        end
+      end
+
+      if session[:cart_id] == nil
+        @current_cart = Cart.create
+        session[:cart_id] = @current_cart.id
       end
     end
 
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,23 +2,23 @@ class OrderItemsController < ApplicationController
     def create
         # Find associated product and current order
         chosen_product = Product.find(params[:product_id])
-        current_order = @current_order
-      
+        current_cart = @current_cart
+        byebug
         # If order already has this product then find the relevant order_item and iterate quantity otherwise create a new order_item for this product
-        if current_order.products.include?(chosen_product)
+        if current_cart.products.include?(chosen_product)
           # Find the order_item with the chosen_product
-          @order_item = current_order.order_items.find_by(:product_id => chosen_product)
+          @order_item = current_cart.order_items.find_by(:product_id => chosen_product)
           # Iterate the order_item's quantity by one
           @order_item.quantity += 1
         else
           @order_item = OrderItem.new
-          @order_item.order = current_order
+          @order_item.cart = current_cart
           @order_item.product = chosen_product
         end
       
         # Save and redirect to order show path
         @order_item.save
-        redirect_to order_path(current_order)
+        redirect_to cart_path(current_cart)
       end
 
       def destroy
@@ -43,4 +43,8 @@ class OrderItemsController < ApplicationController
         redirect_to cart_path(@current_cart)
       end
       
+      private
+        def order_item_params
+          params.require(:order_item).permit(:quantity, :product_id, :cart_id)
+        end
 end
