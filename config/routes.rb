12fa5dc88de803605941caffc44c39b83c9f1f26
redebron20 <redebron20@gmail.com>
2021-04-29 21:37:59 -0400

    Create controller show action and route for cart.

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   resources :products
   resources :admins
   resources :orders
+  resources :carts, only: :show
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
