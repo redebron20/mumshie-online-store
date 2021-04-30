307cbea4ef667fe4eb20b435a46a8bfa4f459fed
redebron20 <redebron20@gmail.com>
2021-04-29 21:28:46 -0400

    Add current_order action to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
     add_flash_types :success, :error, :warning, :info
     include ApplicationHelper
+
+    def current_order
+        if current_user
+          if current_user.orders.exists?(user_id: current_user.id)
+            Order.where(user_id: current_user.id).last
+          else
+            Order.new(user_id: current_user.id)
+          end
+        elsif !session[:order_id].nil?
+          Order.find(session[:order_id])
+        else
+          Order.new
+        end
+      end
 end
